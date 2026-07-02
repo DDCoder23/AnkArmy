d215916091196e67f375b8d0e8cfa044a380afdb
DDCoder23 <DDCoder23@users.noreply.github.com>
2026-07-02 13:30:57 +0200

    modif

--- state.py
+++ state.py
@@ -6,6 +6,7 @@ DEBUG_PATH = os.path.join(os.path.dirname(__file__), "debug.log")
 
 def load_player()-> Player:
     if not os.path.exists(SAVE_PATH) or os.path.getsize(SAVE_PATH) == 0:
+        print(os.path.getsize(SAVE_PATH))
         print('fichier inexistant ou vide')
         return Player()
     
